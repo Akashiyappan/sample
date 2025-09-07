4d3c46c75a6d617e86de7d1fa12c3dcc9509a1c9
Akashiyappan <akashiyappan170@gmail.com>
2025-09-07 21:44:54 +0530

    last update

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,11 @@ import Props from "./component/props";
 import StateExample from "./component/state";
 import MsgChange from "./component/onchangemsgchange";
 import List from "./component/list"
+import TaskManager from "./taskmanager/taskmanager";
 
 function App() {
 
-  return (<>
-    <HelloWorld />
-    <Hello />
-    <Css />
-    <Props title="this is title"/>
-    <StateExample /> <br/><br/>
-    <MsgChange />
-    <List />
-  </>
-  );
+  return <TaskManager />;
 } 
 
 export default App;
